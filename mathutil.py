2230bcc3394019e9e2a276cf13c4b5c41f5e5221
maxis <maxis1718@gmail.com>
2014-03-02 19:13:26 +0800

    add mathutil, including Entropy and SD

---
+++ mathutil.py
@@ -0,0 +1,10 @@
+import math
+def entropy(X, b=2):
+	S = float(sum(X))
+	return -1.0*sum([x/S*math.log(x/S, b) for x in X if x/S > 0.0])
+
+
+def standard_deviation(X):
+	N = float(len(X))
+	miu = float(sum(X))/N
+	return math.sqrt(sum([(x-miu)**2 for x in X])/N)
