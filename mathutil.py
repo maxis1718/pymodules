e0ba4fd228a25292bf0d18fdbd78a77b14bf97a7
Maxis <maxis1718@gmail.com>
2014-03-20 13:45:41 +0800

    add normalize, avg functions

--- mathutil.py
+++ mathutil.py
@@ -1,10 +1,17 @@
 import math
+
 def entropy(X, b=2):
 	S = float(sum(X))
 	return -1.0*sum([x/S*math.log(x/S, b) for x in X if x/S > 0.0])
 
+def normalize(v): 
+	return v if sum(v) == 0 else map( lambda x:x/float(sum(v)), v)
 
-def standard_deviation(X):
-	N = float(len(X))
-	miu = float(sum(X))/N
-	return math.sqrt(sum([(x-miu)**2 for x in X])/N)
+def standard_deviation(v, normalized=False):
+	v = normalize(v) if normalized else v
+	N = float(len(v))
+	miu = float(sum(v))/N
+	return math.sqrt(sum([(x-miu)**2 for x in v])/N)
+
+def avg(v):
+	return sum(v)/float(len(v))	
