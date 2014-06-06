85d221e3f33eabe1de529b9cebf9fee1437c008f
Maxis <maxis1718@gmail.com>
2014-06-06 22:16:15 +0800

    Color V2.0, support text/abbr/code multitype input

--- color.py
+++ color.py
@@ -1,54 +1,77 @@
 # -*- coding: utf-8 -*-
 
-# python string color rendering v1.0
+# python string color rendering v2.0
 
 # [function]
 ## render a string with pre-specified color
 #
 # [usage]
 ## import color
-## color.render('your msg', '<some color name>')
+## color.render('your msg', [color code / EnglishName / Eng abbr.], [light=True/False])
 
-# by Maxis / Jan 28, 2013
+# by Maxis / Jun 6, 2014
 
-_R = '\033[0;32;31m'
-_G = '\033[0;32;32m'
-_B = '\033[0;32;34m'
-_Y = '\033[1;33m'
-_N = '\033[m'
-_LP = '\033[1;35m'
-_LC = '\033[1;36m'
-_W = '\033[1;37m'
+import re
 
-_ColorMap = {
-        'red':_R,
-        'green':_G,
-        'blue':_B,
-        'yellow':_Y,
-        'none':_N,
-        'lightpurple':_LP,
-        'lightcyron':_LC,
-        'white':_W
+ColorMap = {
+	'red': 		31,
+	'green': 	32,
+	'yellow': 	33,
+	'blue': 	34,
+	'purple': 	35,
+	'cyan': 	36,
+	'white':	37
 }
 
-_Alias = {
-        'r':'red',
-        'g':'green',
-        'b':'blue',
-        'y':'yellow',
-        'n':'none',
-        'lp':'lightpurple',
-        'lc':'lightcyron',
-        'w':'white'
-}
+def render(string, this_color, light=False):
+	this_color = str(this_color).lower()
+	if re.match(r'^[a-z]+$', this_color):
+		if this_color.startswith('l') and len(this_color) == 2: # lg
+			light = True
+			this_color = this_color[1:]
 
-def render(text, c):
-        c = c.lower()
-        if c in _Alias: c = _Alias[c]
-        c = _ColorMap['none'] if c not in _ColorMap else _ColorMap[c]
-        return c+text+_ColorMap['none']
+		if this_color.startswith('light'):
+			light = True
+			this_color = this_color.replace('light','')
 
+		if this_color in ColorMap: 
+			code = str(ColorMap[this_color])
+		else:
+			abbr = [ColorMap[c] for c in ColorMap if c.startswith(this_color[0])]
+			if abbr:
+				code = str(abbr[0])
+			else:
+				code = False
+	elif re.match(r'[0-9]+;?', this_color):
+		if this_color.startswith('1;'):
+			light = True
+			this_color = this_color.replace('1;','')
+		code = this_color
+	else:
+		code = False
+	if not code: return string
+	else: 
+		code = '1;'+code if light else code
+		colorized = "\033[" + code + "m" + string + "\033[0m"
+
+	return colorized
+
+def show():
+	for i in range(30, 38):
+		c = str(i)
+		print('This is %s' % render('color ' + c, c))
+		print('This is %s' % render('color ' + c, c, light=True))
+
+	print '--'*8
+
+	for i in range(256):
+		c = '38;05;%d' % i
+		print( render('color '+c, c) )	
 
 if __name__ == '__main__':
+	
+	print render('Hello World', 'lg', light=True)
+	show()
 
-        print render('Hello','Green'), render('World','lp'), render('!!','rEd')
+
+
